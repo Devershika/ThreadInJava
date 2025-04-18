b91841d9fcaed1c5f4d94554daad6c7411fd68cc
Devershika Mohane <142586782+Devershika@users.noreply.github.com>
2025-04-18 22:08:02 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,13 @@
+// Name: DEVERSHIKA MOHANE
+// PRN: 23070126032
+// Batch: AIML A2
+
+public class Main {
+    public static void main(String[] args) {
+        // Starting threads using separate operations
+        ThreadOperations operations = new ThreadOperations();
+
+        operations.runThreadClass();
+        operations.runRunnableClass();
+    }
+}
